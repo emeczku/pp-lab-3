79e528b29cf5e2f290e7ee6dfe2fe66e933ba975
Przemek Majewski <dzius89@gmail.com>
2024-04-07 14:26:43 +0200

    create geometry package && move Point.java to geometry package && create Circle class under geometry package with logic && use Circle class

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
+import geometry.Circle;
+import geometry.Point;
+
 public class Main {
     public static void main(String[] args) {
         Point point = new Point(2.5, 3.5);
+        Circle circle = new Circle(point, 5.0);
 
         point.setX(1.5);
         point.setY(2.5);
 
         System.out.println(point.getX());
         System.out.println(point.getY());
+
+        System.out.println(circle.getCenter());
+        System.out.println(circle.getRadius());
+        System.out.println(circle.calculatePerimeter());
+        System.out.println(circle.getArea());
     }
 }
 

c3762ebb7cc259f8dc258975fe300ea9b74ea80c
Przemek Majewski <dzius89@gmail.com>
2024-04-07 14:03:23 +0200

    create main and point classes && create point logic to set and display properties of that class && use it in main class

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(2.5, 3.5);
+
+        point.setX(1.5);
+        point.setY(2.5);
+
+        System.out.println(point.getX());
+        System.out.println(point.getY());
+    }
+}
+
+

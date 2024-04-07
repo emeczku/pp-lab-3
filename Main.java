553e4fe5144d762f4193e548f0239c1b779e1a8b
Przemek Majewski <dzius89@gmail.com>
2024-04-07 14:52:50 +0200

    create array of circles and colored circles && iterate through it and display proper properties

--- Main.java
+++ Main.java
@@ -7,6 +7,12 @@ public class Main {
         Point point = new Point(2.5, 3.5);
         Circle circle = new Circle(point, 5.0);
         ColoredCircle coloredCircle = new ColoredCircle(point, 5.0, "Blue");
+        Circle[] circles = new Circle[5];
+        circles[0] = new Circle(new Point(0, 0), 1);
+        circles[1] = new Circle(new Point(1, 1), 2);
+        circles[2] = new ColoredCircle(new Point(2, 2), 3, "Yellow");
+        circles[3] = new ColoredCircle(new Point(3, 3), 4, "Blue");
+        circles[4] = new ColoredCircle(new Point(4, 4), 5, "Green");
 
         point.setX(1.5);
         point.setY(2.5);
@@ -20,6 +26,14 @@ public class Main {
         System.out.println(circle.getArea());
 
         System.out.println(coloredCircle.getColor());
+
+        for (Circle c : circles) {
+            System.out.println(c.calculatePerimeter());
+
+            if (c instanceof ColoredCircle) {
+                System.out.println(((ColoredCircle) c).getColor());
+            }
+        }
     }
 }
 
